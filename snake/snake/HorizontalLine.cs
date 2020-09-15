5fdea261ed3fa8786d94e0ccbdfb5de404c45597
sarana <sarana.rb.ru@gmail.com>
2020-09-15 15:49:22 +0700

    Горизонтальные линии

---
+++ snake/snake/HorizontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xLeft; x <= xReight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+
+        public void  Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
