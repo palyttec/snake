ba82ec968539ede327e15a7263af3bf07f77298e
sarana <sarana.rb.ru@gmail.com>
2020-09-17 19:34:51 +0700

    Способность змейки есть еду

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -26,15 +26,31 @@ namespace snake
             Snake snake = new Snake( p, 4, Direction.RIGHT );
             snake.Drow();
 
-            while(true)
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
             {
-                if(Console.KeyAvailable)
+                if (snake.Eat(food))
                 {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
                 }
                 Thread.Sleep(100);
-                snake.Move();
+
+
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey();
+                        snake.HandleKey(key.Key);
+                    }
+                }
             }
         }
     }
