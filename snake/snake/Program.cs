b15cdf74b5cc6f3d689a017782cefe4339c9cc82
sarana <sarana.rb.ru@gmail.com>
2020-09-18 14:41:48 +0700

    Столкновение с хвостом и стенками

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,28 +10,25 @@ namespace snake
         {
             
 
-            //Отрисовка рамочки 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
-
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake( p, 4, Direction.RIGHT );
-            snake.Drow();
+            snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
             food.Draw();
 
+
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
@@ -41,7 +38,7 @@ namespace snake
                 {
                     snake.Move();
                 }
-                Thread.Sleep(100);
+                Thread.Sleep(80);
 
 
                 {
